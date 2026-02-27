7e6b189cd489b5cf09e46ff0f43b65b0f4febb8f
Praveen-tej <tejpraveen2020@gmail.com>
2026-02-27 19:33:29 +0530

    Added Day-1 Greeting Card Project

---
+++ Day-1/greeting-card/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState("");
+  const [cardType, setCardType] = useState("");
+  return (
+    <div>
+      <label htmlFor="name">Enter Name:</label>
+      <input
+        id="name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br/>
+      <label htmlFor="cardType">Greeting Type:</label>
+      <select
+        onChange={(e) => {
+          setCardType(e.target.value)}}>
+        <option value=" ">Select</option>    
+        <option value="birthday">Birthday</option>
+        <option value="congrats">Congrats</option>
+      </select>
+      <br />
+
+      {cardType === "birthday" && (
+        <div className="happyBirthday">
+          <h1> 🎉 Happy Birthday !! 🎂</h1>
+          <h2>{name}</h2>
+        </div>
+      )}
+
+      {cardType === "congrats" && (
+        <div className="congrats">
+          <h1>🎊 Congratulations 🎊</h1>
+          <h2>{name}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App
