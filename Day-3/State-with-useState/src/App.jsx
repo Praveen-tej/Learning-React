c3ee330ed7db365266d7d754aa6c8d17f2c0f323
Praveen-tej <tejpraveen2020@gmail.com>
2026-03-07 06:20:06 +0530

    Interactive counter with use State

--- Day-3/State-with-useState/src/App.jsx
+++ Day-3/State-with-useState/src/App.jsx
@@ -6,28 +6,24 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const  increment = () => {
+    setCount(count+1)
+  }
+
+  const decrement = () => {
+    setCount(count-1)
+  }
+ 
+  const reset = () => {
+    setCount(0)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset} >Reset</button>
+    <div>Count:{count}</div>
     </>
   )
 }
